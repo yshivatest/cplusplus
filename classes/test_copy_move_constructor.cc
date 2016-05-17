412c3e7dfb9397b394817f4730ebdbc592441a06
syerramreddy <syerramreddy@machinezone.com>
2016-05-17 23:58:14 +0000

    add some more about universal references

--- classes/test_copy_move_constructor.cc
+++ classes/test_copy_move_constructor.cc
@@ -7,17 +7,50 @@
 
 #include<iostream>
 using namespace std;
-
 class Siva {
 public:
 	int x_;
 	Siva(int x) : x_{x} {}
 	Siva(Siva& s) {cout<<"Copy constructor called\n"; x_ = s.x_;}
-	Siva(Siva&& s) {cout<<"move constructor called"<<x_<<endl;x_ = s.x_; cout<<"this="<<this<<",s="<<&s;}
+	Siva(Siva&& s) {cout<<"move constructor called"<<x_<<endl;x_ = s.x_; cout<<"this="<<this<<",s="<<&s<<endl;}
 	Siva& operator=(Siva&& s) {cout<<"move assignment= called"<<x_<<","<<s.x_<<endl;return *this;}
 	Siva& operator=(Siva& s) {cout<<"Assignment operator called\n"; x_ = s.x_; return *this;}
 	friend ostream& operator<<(ostream&, const Siva&);
 };
+
+class Krishna {
+    public:
+        Krishna() {}
+        Krishna(Krishna& k){}
+        Krishna(Krishna&& k){}
+};
+void temp(int &x, Siva &y) {
+    cout<<"reference temp called\n";
+}
+
+void temp(int&& x, Siva&& y) {
+    cout<<"move temp called\n";
+}
+
+void temp2(Krishna& x, Siva &y) {
+    cout<<"reference temp2 called\n";
+}
+
+void temp2(Krishna &&x, Siva &&y) {
+    cout<<"move temp2 called\n";
+}
+
+#if 0
+template<typename T1, typename T2>
+void template_temp(T1 &x, T2 &&y)
+{
+}
+#endif
+template<typename T1, typename T2>
+void template_temp(T1 &&x, T2 &&y)
+{
+}
+
 ostream& operator<<(ostream& o, const Siva& s)
 {
 	cout<<"output operator called\n";
@@ -39,6 +72,7 @@ Siva voodoo(Siva x)
 	cout<<"voodoo called"<<&x<<endl;
 	return x;
 }
+int g_x = 9;
 int main()
 {
 	Siva x(3);
@@ -48,6 +82,11 @@ int main()
 	foo(z);
 	bar(x);
 	z = voodoo(x);
-	cout<<z;
+	//cout<<z;
 	Siva a = voodoo(x);
+    //temp(1, a);
+    temp(std::move(g_x), std::move(a));
+    Krishna k;
+    temp2(k, std::move(a));
+    template_temp(k, std::move(a));
 }
